d28ab9c910929f67c2edda59fc8277db833a0da7
James Thompson <keiouu@gmail.com>
2013-03-23 15:01:06 +0000

    Force / to /index.html

--- server/api.js
+++ server/api.js
@@ -5,10 +5,15 @@
 
 var db = require("./db.js");
 
-exports.createPage = function (url, author, template, content) {
+exports.createPage = function (location, author, template, content) {
+
+	if (location.substr(-1) === "/") {
+		location = location + "index.html";
+	}
+
 	var page = new db.models.Page({
 		version: 1,
-		url: url,
+		url: location,
 		author: author,
 		template: template,
 		data: content,
@@ -17,21 +22,29 @@ exports.createPage = function (url, author, template, content) {
 
 	page.save(function (err) {
 		if (err) {
-			console.log("Error saving page: " + url);
+			console.log("Error saving page: " + location);
 		} else {
-			console.log("Created page: " + url);
+			console.log("Created page: " + location);
 		}
 	});
+
 };
 
 exports.getPage = function (location, callback) {
+
+	if (location.substr(-1) === "/") {
+		location = location + "index.html";
+	}
+
 	var query = db.models.Page.findOne({
 		url: location
 	});
+
 	query.exec(function (err, data) {
 		if (!err && !data) {
 			err = "404 - Page cannot be found";
 		}
 		callback(err, data);
 	});
+
 };
